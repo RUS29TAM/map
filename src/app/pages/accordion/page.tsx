5009a6219937ec558719eb6c55d25f5c0f011aad
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-07-13 23:24:13 +0300

    chore: add accordion

---
+++ src/app/pages/accordion/page.tsx
@@ -0,0 +1,260 @@
+'use client'
+import React from 'react';
+import styles from './accordion.module.css'
+
+const Page = () => {
+    return (
+        <div className={styles.wrapper}>
+            <section className={styles.section}>
+                <div className={styles.container}>
+                    <div className={styles.accordion}>
+                        <div className={styles.accordionItem} id="question1">
+                            <a className={styles.accordionLink} href="#question1">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>1 BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question2">
+                            <a className={styles.accordionLink} href="#question2">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question3">
+                            <a className={styles.accordionLink} href="#question3">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question4">
+                            <a className={styles.accordionLink} href="#question4">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question5">
+                            <a className={styles.accordionLink} href="#question5">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question6">
+                            <a className={styles.accordionLink} href="#question6">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question7">
+                            <a className={styles.accordionLink} href="#question7">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question8">
+                            <a className={styles.accordionLink} href="#question8">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question9">
+                            <a className={styles.accordionLink} href="#question9">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question10">
+                            <a className={styles.accordionLink} href="#question10">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                        <div className={styles.accordionItem} id="question11">
+                            <a className={styles.accordionLink} href="#question11">
+                                <div className={styles.flex}>
+                                    <h3 className={styles.h3}>END BRANDING</h3>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.li}>#Figma</li>
+                                        <li className={styles.li}>#Sketch</li>
+                                        <li className={styles.li}>#Adobe</li>
+                                        <li className={styles.li}>#Invision</li>
+                                        <li className={styles.li}>#Protopie</li>
+                                    </ul>
+                                </div>
+                                <i className={`${styles.icon} ${styles.ionMdArrowForward}`}></i>
+                                <i className={`${styles.icon} ${styles.ionMdArrowDown}`}></i>
+                            </a>
+                            <div className={`${styles.answer}`}>
+                                <p className={`${styles.p}`}> We believe in the greater good, we strive to do something for people, we aim to make
+                                    their lives easier and more enjoyable, we love businesses that keep this</p>
+                            </div>
+                            <hr className={styles.hr}/>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Page;
